2409f94097d5eeba2fc4cd8f1cb70358ec989891
Mauri Twerda <mauritwerda@MacBook-Air-de-Mauri.local>
2024-03-15 23:48:34 -0300

    Configuraciones iniciales - Fondo - Fuentes

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import { Encode_Sans_Expanded } from "next/font/google";
+import "./globals.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+const encodeSansFont = Encode_Sans_Expanded({ weight: "400", subsets: ["latin"] });
+
+export const metadata = {
+  title: "Puerto Montt ecommerce",
+  description: "Ecommerce generated to learn NextJS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={encodeSansFont.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <div>
+      <nav className='nav-custom'></nav>
+      <h1>Nosotros</h1>
+    </div>
+  );
+}
